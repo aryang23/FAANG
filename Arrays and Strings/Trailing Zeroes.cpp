186b66980721812210d6b4d37fb8c9bb13fcdfbe
Aryan Grover <58443700+aryang23@users.noreply.github.com>
2020-07-24 22:49:00 +0530

    Trailing Zeroes

---
+++ Arrays and Strings/Trailing Zeroes.cpp
@@ -0,0 +1,15 @@
+//Trailing Zeroes
+//By Aryan Grover(@aryang23)
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n,ans=0;
+    cin>>n;
+    while(n>0)
+    {
+        n=n/5;
+        ans+=n;
+    }
+    cout<<ans;
+}
